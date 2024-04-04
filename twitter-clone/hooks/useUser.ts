b868226423621da6bd0f43862c6b69abdee332ed
davitvarshanidze <davittvarshanidze@gmail.com>
2024-04-04 16:31:30 +0400

    adding swr

--- twitter-clone/hooks/useUser.ts
+++ twitter-clone/hooks/useUser.ts
@@ -1,9 +1,5 @@
-import useSWR from 'swr';
-
 import fetcher from '@/libs/fetcher';
 
-const useUser = (userId: string) => {
-  const { data, error, isLoading, mutate } = useSWR(userId ? `/api/users/${userId}` : null, fetcher);
 
   return {
     data,
