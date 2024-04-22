4c97add844231c93aad898d300fad18ebf3b9111
dvarshanidze <davittvarshanidze@gmail.com>
2024-04-22 15:12:14 +0400

    cc

--- twitter-clone/components/Avatar.tsx
+++ twitter-clone/components/Avatar.tsx
@@ -3,7 +3,6 @@ import { useRouter } from "next/router";
 import { useCallback } from "react";
 
 import useUser from "@/hooks/useUser";
-// av
 interface AvatarProps {
   userId: string;
   isLarge?: boolean;
