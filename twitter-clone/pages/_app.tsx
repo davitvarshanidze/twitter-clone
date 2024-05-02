ee54dce2bd133e6312747839d66316decc0173a9
dvarshanidze <davittvarshanidze@gmail.com>
2024-05-02 15:40:20 +0400

    cc

--- twitter-clone/pages/_app.tsx
+++ twitter-clone/pages/_app.tsx
@@ -1,13 +1,13 @@
-import type { AppProps } from 'next/app'
-import { Toaster } from 'react-hot-toast';
-import { SessionProvider } from 'next-auth/react';
-import { useSession } from "next-auth/react"
+import type { AppProps } from "next/app";
+import { Toaster } from "react-hot-toast";
+import { SessionProvider } from "next-auth/react";
+import { useSession } from "next-auth/react";
 
-import Layout from '@/components/Layout'
-import LoginModal from '@/components/modals/LoginModal'
-import RegisterModal from '@/components/modals/RegisterModal'
-import '@/styles/globals.css'
-import EditModal from '@/components/modals/EditModal';
+import Layout from "@/components/Layout";
+import LoginModal from "@/components/modals/LoginModal";
+import RegisterModal from "@/components/modals/RegisterModal";
+import "@/styles/globals.css";
+import EditModal from "@/components/modals/EditModal";
 
 // const {data:session} = useSession();
 
@@ -22,5 +22,5 @@ export default function App({ Component, pageProps }: AppProps) {
         <Component {...pageProps} />
       </Layout>
     </SessionProvider>
-  )
+  );
 }
